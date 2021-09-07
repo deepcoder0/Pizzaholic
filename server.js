f1af2a57b7f5bcdcc7b192c1051610040499d62e
deepcoder0 <deepanshu.agarwal2456@gmail.com>
2021-09-07 10:50:48 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.render('home') // file_name inside views.
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(PORT , () => {
+    console.log('Listening on port 3000');
+})
