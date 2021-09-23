d3cf96c2b009f3044028b31da21ca6b8ed11e5a2
deepcoder0 <deepanshu.agarwal2456@gmail.com>
2021-09-23 08:44:28 +0530

    Add cart, auth pages and minor other issues.

--- server.js
+++ server.js
@@ -6,17 +6,32 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000
 
-//Assets
-app.use(express.static('public'));
+// Assets 
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home') // file_name inside views.
-})
 
+// set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.listen(PORT , () => {
-    console.log('Listening on port 3000');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
